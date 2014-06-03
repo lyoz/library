0bd5b31557dffdc4e06993948703c6fd0ec2af4a
lyoz <lyoz@users.noreply.github.com>
2014-06-03 14:34:13 +0900

    modify scc.cpp

--- graph/scc.cpp
+++ graph/scc.cpp
@@ -1,11 +1,10 @@
-// Verify: POJ 2186, AOJ 2505
+// Verify: POJ 2186, AOJ 2222, AOJ 2505
 
 void DFS(const Graph& g,int v,vi& vis,vi& order)
 {
 	if(vis[v]) return;
 	vis[v]=1;
-	foreach(e,g[v])
-		DFS(g,e->dst,vis,order);
+	for(auto e:g[v]) DFS(g,e.dst,vis,order);
 	order.push_back(v);
 }
 
@@ -15,8 +14,8 @@ vvi SCC(const Graph& g)
 	vi vis(n),order;
 	rep(i,n) DFS(g,i,vis,order);
 	Graph t(n);
-	rep(i,n) foreach(e,g[i])
-		t[e->dst].push_back(Edge(e->dst,e->src));
+	rep(i,n) for(auto e:g[i])
+		t[e.dst].emplace_back(e.dst,e.src);
 	fill(all(vis),0);
 	vvi res;
 	per(i,n) if(!vis[order[i]]){
