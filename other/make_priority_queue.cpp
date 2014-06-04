3c0d1eebf210adb1ac8add420b45e33cec7f35b1
lyoz <lyoz@users.noreply.github.com>
2014-06-05 00:43:14 +0900

    add make_priority_queue.cpp

---
+++ other/make_priority_queue.cpp
@@ -0,0 +1,15 @@
+// https://ideone.com/TimImn
+
+template<typename Functor>
+struct functor_traits{
+	template<typename C,typename Ret,typename Arg,typename... Args>
+		static Arg helper(Ret(C::*)(Arg,Args...));
+	template<typename C,typename Ret,typename Arg,typename... Args>
+		static Arg helper(Ret(C::*)(Arg,Args...)const);
+	using first_argument_type=decltype(helper(&Functor::operator()));
+};
+
+template<typename Compare,typename T=typename functor_traits<Compare>::first_argument_type>
+priority_queue<T,vector<T>,Compare> make_priority_queue(Compare comp){
+	return priority_queue<T,vector<T>,Compare>(move(comp));
+}
