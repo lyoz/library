cd83c376b3572d4db3ce91e6971ce908e3007dfb
lyoz <lyoz@users.noreply.github.com>
2014-10-26 05:06:58 +0900

    古かったので書きなおした

--- flow/mincostflow.cpp
+++ flow/mincostflow.cpp
@@ -1,67 +1,62 @@
-// Verify: PKU 2135(297ms), PKU 2195(157ms), UVA 10594(int→ll,344ms),
-//         SRM506Div1 Medium, SRM611 GoodCompanyDivOne
+// Verify: UVA 10594(int→ll,248ms), SRM 637 GreaterGame
 
 struct Edge{
-	int src,dst,cap,cost;
+	int src,dst,cost,cap,flow;
 	Edge(){}
-	Edge(int s,int d,int ca,int co):src(s),dst(d),cap(ca),cost(co){}
+	Edge(int s,int d,int co,int ca=0,int f=0):src(s),dst(d),cost(co),cap(ca),flow(f){}
 };
-typedef vector<vector<Edge> > Graph;
-
-int MinCostFlow(const Graph& _g,int begin,int end,int flow)
-{
-	int size=_g.size();
-	Graph g(size);
-	vvi rev(size);
-	rep(i,size) foreach(e,_g[i]){
-		g[i].push_back(*e);
-		g[e->dst].push_back(Edge(e->dst,e->src,0,-e->cost));
-		rev[i].push_back(g[e->dst].size()-1);
-		rev[e->dst].push_back(g[i].size()-1);
+struct Graph{
+	vector<Edge> es;
+	vi head,next;
+	Graph(){}
+	Graph(int n):head(n,-1){}
+	// コスト:-co,容量:0の逆辺も追加する
+	void AddEdge(int u,int v,int co,int ca){
+		es.emplace_back(u,v,co,ca); next.push_back(head[u]); head[u]=es.size()-1;
+		es.emplace_back(v,u,-co,0); next.push_back(head[v]); head[v]=es.size()-1;
 	}
+};
+
+int MinCostFlow(Graph& g,int tap,int sink,int flow)
+{
+	int n=g.head.size();
 	
 	int res=0;
-	vi pots(size);
+	vi pots(n);
 	while(flow){
-		vi dist(size,INFTY);
-		vector<pii> prev(size,mp(-1,-1));
-		priority_queue<pii,vector<pii>,greater<pii> > pq;
-		pq.push(mp(0,begin));
-		dist[begin]=0;
-		while(!pq.empty()){
-			int curd=pq.top().first,curv=pq.top().second; pq.pop();
-			if(dist[curv]<curd)
-				continue;
-			rep(i,g[curv].size()){
-				Edge e=g[curv][i];
-				if(e.cap==0)
-					continue;
-				int newdist=dist[e.src]+e.cost+pots[e.src]-pots[e.dst];
-				if(dist[e.dst]>newdist){
-					dist[e.dst]=newdist;
-					prev[e.dst]=mp(e.src,i);
-					pq.push(mp(newdist,e.dst));
+		vi cost(n,INF),prev(n,-1);
+		priority_queue<pii,vector<pii>,greater<pii>> pq;
+		cost[tap]=0;
+		pq.emplace(0,tap);
+		while(pq.size()){
+			int c,v; tie(c,v)=pq.top(); pq.pop();
+			if(c>cost[v]) continue;
+			for(int i=g.head[v];i!=-1;i=g.next[i]){
+				Edge e=g.es[i];
+				if(e.cap-e.flow==0) continue;
+				if(cost[e.dst]>cost[e.src]+e.cost+pots[e.src]-pots[e.dst]){
+					cost[e.dst]=cost[e.src]+e.cost+pots[e.src]-pots[e.dst];
+					prev[e.dst]=i;
+					pq.emplace(cost[e.dst],e.dst);
 				}
 			}
 		}
 		
-		if(prev[end].first==-1)
-			return -1;
-		rep(i,size)
-			pots[i]+=dist[i];
+		if(cost[sink]==INF) return -1;
+		rep(i,n) pots[i]+=cost[i];
 		
-		int f=flow;
-		for(int v=end;v!=begin;v=prev[v].first){
-			Edge e=g[prev[v].first][prev[v].second];
-			f=min(f,e.cap);
+		int augment=flow;
+		for(int v=sink;v!=tap;v=g.es[prev[v]].src){
+			Edge e=g.es[prev[v]];
+			augment=min(augment,e.cap-e.flow);
 		}
-		for(int v=end;v!=begin;v=prev[v].first){
-			int i=prev[v].first,j=prev[v].second;
-			g[i][j].cap-=f;
-			g[g[i][j].dst][rev[i][j]].cap+=f;
+		for(int v=sink;v!=tap;v=g.es[prev[v]].src){
+			int i=prev[v];
+			g.es[i].flow+=augment;
+			g.es[i^1].flow-=augment;
 		}
-		flow-=f;
-		res+=f*pots[end];
+		flow-=augment;
+		res+=augment*pots[sink];
 	}
 	
 	return res;
