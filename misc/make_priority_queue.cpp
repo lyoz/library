4b1dc18e77d52ad369d83f3af9703df2ff5e7778
lyoz <lyoz@users.noreply.github.com>
2014-12-17 05:38:20 +0900

    update make_priority_queue

--- misc/make_priority_queue.cpp
+++ misc/make_priority_queue.cpp
@@ -1,15 +1,12 @@
-// https://ideone.com/TimImn
-
-template<typename Functor>
-struct functor_traits{
-	template<typename C,typename Ret,typename Arg,typename... Args>
-	static Arg helper(Ret(C::*)(Arg,Args...));
-	template<typename C,typename Ret,typename Arg,typename... Args>
-	static Arg helper(Ret(C::*)(Arg,Args...)const);
-	using first_argument_type=decltype(helper(&Functor::operator()));
+template<typename T>
+struct functor_traits:public functor_traits<decltype(&T::operator())>{};
+template<typename C,typename Ret,typename... Args>
+struct functor_traits<Ret(C::*)(Args...)const>{
+	template<size_t I>
+	using nth_argument_type=typename tuple_element<I,tuple<Args...>>::type;
+	using first_argument_type=nth_argument_type<0>;
 };
-
-template<typename Compare,typename T=typename functor_traits<Compare>::first_argument_type>
+template<typename Compare,typename T=typename decay<typename functor_traits<Compare>::first_argument_type>::type>
 priority_queue<T,vector<T>,Compare> make_priority_queue(Compare comp){
-	return priority_queue<T,vector<T>,Compare>(move(comp));
+	return priority_queue<T,vector<T>,Compare>(comp);
 }
