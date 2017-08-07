90d121b348745cbf3d9bb39f53b7fab9e71a23f5
lyoz <lyoz@users.noreply.github.com>
2017-08-07 15:53:47 +0900

    modify make_priority_queue

--- misc/make_priority_queue.cpp
+++ misc/make_priority_queue.cpp
@@ -1,12 +1,5 @@
-template<typename T>
-struct functor_traits:public functor_traits<decltype(&T::operator())>{};
-template<typename C,typename Ret,typename... Args>
-struct functor_traits<Ret(C::*)(Args...)const>{
-	template<size_t I>
-	using nth_argument_type=typename tuple_element<I,tuple<Args...>>::type;
-	using first_argument_type=nth_argument_type<0>;
-};
-template<typename Compare,typename T=typename decay<typename functor_traits<Compare>::first_argument_type>::type>
-priority_queue<T,vector<T>,Compare> make_priority_queue(Compare comp){
+template<typename T,typename Compare>
+auto make_priority_queue(const Compare& comp)
+{
 	return priority_queue<T,vector<T>,Compare>(comp);
 }
