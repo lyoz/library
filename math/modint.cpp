754f4c0bab893117c9e053160737fe95acb0cd95
lyoz <lyoz@users.noreply.github.com>
2017-08-26 14:32:42 +0900

    fix modint

---
+++ math/modint.cpp
@@ -0,0 +1,38 @@
+template<unsigned M>
+struct modint{
+	unsigned val;
+	modint(unsigned x):val((x%M+M)%M){}
+	modint pow(unsigned r)const{
+		ll a=val,x=1;
+		for(;r;r>>=1){
+			if(r&1)
+				(x*=a)%=M;
+			(a*=a)%=M;
+		}
+		return x;
+	}
+	modint inv()const{
+		unsigned x=1;
+		for(unsigned a=val,b=M,u=0;b;){
+			ll t=a/b;
+			swap(a-=b*t,b);
+			swap(x-=u*t,u);
+		}
+		return x;
+	}
+	modint& operator=(const modint& x)&{val=x.val; return *this;}
+	modint& operator+=(const modint& x)&{if((val+=x.val)>=M) val-=M; return *this;}
+	modint& operator-=(const modint& x)&{if((val+=M-x.val)>=M) val-=M; return *this;}
+	modint& operator*=(const modint& x)&{val=(ll)val*x.val%M; return *this;}
+	modint& operator/=(const modint& x)&{val=(ll)val*x.inv().val%M; return *this;}
+};
+
+template<unsigned M> bool operator==(const modint<M>& a,const modint<M>& b){return a.val==b.val;}
+template<unsigned M> bool operator!=(const modint<M>& a,const modint<M>& b){return a.val!=b.val;}
+template<unsigned M> modint<M> operator+(modint<M> a,const modint<M>& b){a+=b; return a;}
+template<unsigned M> modint<M> operator-(modint<M> a,const modint<M>& b){a-=b; return a;}
+template<unsigned M> modint<M> operator*(modint<M> a,const modint<M>& b){a*=b; return a;}
+template<unsigned M> modint<M> operator/(modint<M> a,const modint<M>& b){a/=b; return a;}
+template<unsigned M> ostream& operator<<(ostream& os,const modint<M>& x){return os<<x.val;}
+
+using mint=modint<MOD>;
