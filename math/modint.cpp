e5ecd3ca48c2e3cd8fea912aac4f4500f98f8d21
lyoz <lyoz@users.noreply.github.com>
2017-09-24 19:19:53 +0900

    fix modint

--- math/modint.cpp
+++ math/modint.cpp
@@ -1,8 +1,8 @@
-template<unsigned M>
+template<uint M>
 struct modint{
-	unsigned val;
-	modint(unsigned x):val((x%M+M)%M){}
-	modint pow(unsigned r)const{
+	uint val;
+	modint(uint x=0):val((x%M+M)%M){}
+	modint pow(uint r)const{
 		ll a=val,x=1;
 		for(;r;r>>=1){
 			if(r&1)
@@ -25,14 +25,15 @@ struct modint{
 	modint& operator-=(const modint& x)&{if((val+=M-x.val)>=M) val-=M; return *this;}
 	modint& operator*=(const modint& x)&{val=(ll)val*x.val%M; return *this;}
 	modint& operator/=(const modint& x)&{val=(ll)val*x.inv().val%M; return *this;}
+	explicit operator uint()const{return val;}
 };
 
-template<unsigned M> bool operator==(const modint<M>& a,const modint<M>& b){return a.val==b.val;}
-template<unsigned M> bool operator!=(const modint<M>& a,const modint<M>& b){return a.val!=b.val;}
-template<unsigned M> modint<M> operator+(modint<M> a,const modint<M>& b){a+=b; return a;}
-template<unsigned M> modint<M> operator-(modint<M> a,const modint<M>& b){a-=b; return a;}
-template<unsigned M> modint<M> operator*(modint<M> a,const modint<M>& b){a*=b; return a;}
-template<unsigned M> modint<M> operator/(modint<M> a,const modint<M>& b){a/=b; return a;}
-template<unsigned M> ostream& operator<<(ostream& os,const modint<M>& x){return os<<x.val;}
+template<uint M> bool operator==(const modint<M>& a,const modint<M>& b){return a.val==b.val;}
+template<uint M> bool operator!=(const modint<M>& a,const modint<M>& b){return a.val!=b.val;}
+template<uint M> modint<M> operator+(modint<M> a,const modint<M>& b){a+=b; return a;}
+template<uint M> modint<M> operator-(modint<M> a,const modint<M>& b){a-=b; return a;}
+template<uint M> modint<M> operator*(modint<M> a,const modint<M>& b){a*=b; return a;}
+template<uint M> modint<M> operator/(modint<M> a,const modint<M>& b){a/=b; return a;}
+template<uint M> ostream& operator<<(ostream& os,const modint<M>& x){return os<<x.val;}
 
 using mint=modint<MOD>;
